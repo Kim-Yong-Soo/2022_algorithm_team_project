5e41feb07d8e538c9d497c616fd2d67a93d87a92
Kim-Yong-Soo <dydtnalswl@gmail.com>
2022-10-04 13:29:28 +0900

    add the first version of MidProject.java

---
+++ 2022_algorithm_team_project/src/midProejct/MidProject.java
@@ -0,0 +1,62 @@
+package midProejct;
+
+import java.util.HashMap;
+import java.util.Random;
+
+class SLP {
+	private int diceCnt, curPos;
+
+	public SLP() {
+		diceCnt = 0;
+		curPos = 1;
+	}
+
+	public void process(HashMap<Integer, Integer> snakes, HashMap<Integer, Integer> ladders) {
+		Random random = new Random();
+		int diceNum;
+
+		while (curPos + (diceNum = random.nextInt(6) + 1) < 100) {
+			curPos += diceNum;
+			diceCnt++;
+			System.out.print(curPos - diceNum + "에서 " + "주사위 값이 " + diceNum + "이 나와 ");
+			if (snakes.containsKey(curPos)) {
+				curPos = snakes.get(curPos);
+			} else if (ladders.containsKey(curPos)) {
+				curPos = ladders.get(curPos);
+			}
+			System.out.println(curPos + "(으)로 이동했습니다.");
+		}
+	}
+
+	public int getDiceCnt() {
+		return diceCnt;
+	}
+}
+
+public class MidProject {
+
+	public static void main(String[] args) {
+		HashMap<Integer, Integer> snakes = new HashMap<>(), ladders = new HashMap<>();
+
+		snakes.put(56, 4);
+		snakes.put(88, 33);
+		snakes.put(97, 59);
+
+		ladders.put(8, 55);
+		ladders.put(19, 76);
+		ladders.put(32, 92);
+
+		int totalDiceCnt = 0, totalTryCnt = 0;
+		SLP slp;
+
+		while (totalTryCnt < 30) {
+			totalTryCnt++;
+			slp = new SLP();
+			slp.process(snakes, ladders);
+			totalDiceCnt += slp.getDiceCnt();
+		}
+		System.out.println(totalTryCnt + "번 시도한 결과, 총 주사위가 던져진 횟수는 " + totalDiceCnt + "번 이었고, 평균은 "
+				+ String.format("%.2f", totalDiceCnt / (double) (totalTryCnt)) + "입니다.");
+	}
+
+}
